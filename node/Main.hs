ecb053566c19d8e837b298bfdd8d5b4962dd1bfe
Jakub Kopański <jakub@quivade.com>
2018-04-06 13:30:54 +0200

    separate argument parsing

--- node/Main.hs
+++ node/Main.hs
@@ -1,45 +1,57 @@
+{-# language TemplateHaskell #-}
 module Main where
 
-import Data.Semigroup      ((<>))
-import Options.Applicative
+import           Control.Concurrent (threadDelay)
+import           Control.Monad      (forever)
+import qualified Control.Distributed.Backend.P2P as P2P
+import           Control.Distributed.Process
+import           Control.Distributed.Process.Closure
+import           Control.Distributed.Process.Node
+import           Data.Semigroup      ((<>))
+import           Options.Applicative (execParser)
 
-data Config = Config
-  { _send :: Int
-  , _wait :: Int
-  , _seed :: Int
-  } deriving (Eq, Show)
+import Opts (Config, options)
 
-config :: Parser Config
-config = Config <$> send <*> wait <*> seed
+host = "127.0.0.1"
+port = "10501"
 
-send :: Parser Int
-send = option auto
-  ( long "send-for"
- <> short 'k'
- <> metavar "K"
- <> value 30
- <> help "Send messages for K seconds" )
+replyBack :: (ProcessId, String) -> Process ()
+replyBack (sender, msg) = send sender msg
 
-wait :: Parser Int
-wait = option auto
-  ( long "wait-for"
- <> short 'l'
- <> metavar "L"
- <> value 30
- <> help "Wait K seconds for messages" )
+logMessage :: String -> Process ()
+logMessage msg = say $ "handling: " <> msg
 
-seed :: Parser Int
-seed = option auto
-  ( long "with-seed"
- <> short 's'
- <> metavar "S"
- <> value 1234
- <> help "Use S as RNG seed" )
+sampleTask :: (Int, String) -> Process ()
+sampleTask (t, s) = liftIO (threadDelay (t * 100000)) >> say s
+
+remotable ['sampleTask]
+
+myRemoteTable :: RemoteTable
+myRemoteTable = Main.__remoteTable initRemoteTable
 
 main :: IO ()
 main = do
-  execParser opts >>= putStrLn . show
-  where opts = info (config <**> helper)
-          ( fullDesc
-         <> progDesc "IOHK test task"
-         <> header "node - send and receive messages" )
+  execParser options >>= putStrLn . show
+
+  P2P.bootstrap "gallium" "10501" (\port -> ("gallium", port)) myRemoteTable [P2P.makeNodeId "localhost:10501"] (forever $ do
+    liftIO $ threadDelay 100000
+    P2P.nsendPeers "myService" ("some", "message"))
+
+    -- us <- getSelfNode
+    -- _ <- spawnLocal $ sampleTask (1 :: Int, "using spawnLocal")
+    -- pid <- spawn us $ $(mkClosure 'sampleTask) (1 :: Int, "using spawn")
+    -- liftIO $ threadDelay 200000
+
+    -- echo <- spawnLocal $ forever $ do
+    --   receiveWait [match logMessage, match replyBack]
+
+    -- say "send some messages!"
+    -- send echo "hello"
+
+    -- self <- getSelfPid
+    -- send echo (self, ("hello" :: String))
+
+    -- m <- expectTimeout 100000 :: Process (Maybe String)
+    -- case m of
+    --   Nothing -> die "nothing came back!"
+    --   Just s -> say $ "got: " <> s <> " back!"
---
+++ node/Opts.hs
@@ -0,0 +1,43 @@
+module Opts where
+
+import Data.Semigroup      ((<>))
+import Options.Applicative
+
+data Config = Config
+  { _send :: Int
+  , _wait :: Int
+  , _seed :: Int
+  } deriving (Eq, Show)
+
+config :: Parser Config
+config = Config <$> send <*> wait <*> seed
+
+send :: Parser Int
+send = option auto
+  ( long "send-for"
+ <> short 'k'
+ <> metavar "K"
+ <> value 30
+ <> help "Send messages for K seconds" )
+
+wait :: Parser Int
+wait = option auto
+  ( long "wait-for"
+ <> short 'l'
+ <> metavar "L"
+ <> value 30
+ <> help "Wait K seconds for messages" )
+
+seed :: Parser Int
+seed = option auto
+  ( long "with-seed"
+ <> short 's'
+ <> metavar "S"
+ <> value 1234
+ <> help "Use S as RNG seed" )
+
+options = info (config <**> helper)
+  ( fullDesc
+ <> progDesc "IOHK test task"
+ <> header "node - send and receive messages" )
+
